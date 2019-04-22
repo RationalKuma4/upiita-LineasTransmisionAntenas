535bc073bfeb89f845c852fe13455325d1d88be6
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2019-04-22 18:07:39 -0500

    tarea

--- Tarea7/tarea.m
+++ Tarea7/tarea.m
@@ -2,8 +2,8 @@ clear all;
 close all;
 clc;
 
-teta=0:.001:2*pi;
-%% 
+%%
+teta=.001:.001:2*pi+.001;
 F1=(cos(3*pi/2*(cos(teta))))./(sin(teta));
 vMax1=max(F1);
 F1n=(cos(3*pi/2*(cos(teta))))./(sin(teta)).*(1./vMax1);
@@ -12,8 +12,13 @@ figure(1);
 plot(teta,F1,teta,F1n);
 grid on;
 legend('F','Fn');
+title('Coordenadas \ cartesianas','interpreter','latex');
+xlabel('$\theta$','interpreter','latex');
+ylabel('$F$','interpreter','latex');
+
 figure(2);
 polarplot(teta,F1n);
+title('Coordenadas \ polares','interpreter','latex')
 
 %%
 teta2=.001:.001:pi+.001;
@@ -25,6 +30,10 @@ figure(3);
 plot(teta2,F2,teta2,F2n);
 grid on;
 legend('F','Fn');
+title('Coordenadas \ cartesianas','interpreter','latex');
+xlabel('$\theta$','interpreter','latex');
+ylabel('$F$','interpreter','latex');
+
 figure(4);
 polarplot(teta2,F2n);
-
+title('Coordenadas \ polares','interpreter','latex')

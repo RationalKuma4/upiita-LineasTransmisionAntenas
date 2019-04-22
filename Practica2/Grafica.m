ee2a3733bb595a25f70ee34c07d0c973489c0501
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2019-04-22 16:14:44 -0500

    hy

--- Practica2/Grafica.m
+++ Practica2/Grafica.m
@@ -5,50 +5,55 @@ clc;
 %% Antena onda completa
 angulo=0:pi/18:pi;
 angulo2=2*pi:-pi/18:pi;
+angulos=[0 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180];
 
-potenciadBm=[-23 -25 -26 -27 -30 -31 -34 -36 -33 -33 -34 -32 -31 -29 -29 -27 -27 -28 -28]; %[dBm]
-potenciadB=potenciadBm-30;
-potenciaWatts=10.^(potenciadBm./10);
+%potenciadBm=[-23 -25 -26 -27 -30 -31 -34 -36 -33 -33 -34 -32 -31 -29 -29 -27 -27 -28 -28]; %[dBm]
+%potenciadB=potenciadBm-30;
+%potenciaWatts=10.^(potenciadB./10);
 
-figure(1);
-polarplot(angulo,potenciadBm);
-hold on;
-polarplot(angulo2,potenciadBm);
-title('dBm');
-
-figure(2);
-polarplot(angulo,potenciadB);
-hold on;
-polarplot(angulo2,potenciadB);
-title('dB');
-
-figure(3);
-polarplot(angulo,potenciaWatts);
-hold on;
-polarplot(angulo2,potenciaWatts);
-title('Watt');
+%vals=[angulos' potenciadBm' potenciadB' potenciaWatts'];
+% 
+% 
+% figure(1);
+% polarplot(angulo,potenciadBm);
+% hold on;
+% polarplot(angulo2,potenciadBm);
+% title('Patron de radiacion onda completa [dBm]','Interpreter','latex');
+% 
+% figure(2);
+% polarplot(angulo,potenciadB);
+% hold on;
+% polarplot(angulo2,potenciadB);
+% title('Patron de radiacion onda completa [dB]','Interpreter','latex');
+% 
+% figure(3);
+% polarplot(angulo,potenciaWatts);
+% hold on;
+% polarplot(angulo2,potenciaWatts);
+% title('Patron de radiacion onda completa [W]','Interpreter','latex');
 
 %% Antena dipolo doblado
 potenciadBm=[-29 -33 -34 -36 -36 -31 -31 -28 -30 -27 -27 -25 -27 -30 -33 -37 -36 -38 -30]; %[dBm]
 potenciadB=potenciadBm-30;
-potenciaWatts=10.^(potenciadBm./10);
+potenciaWatts=10.^(potenciadB./10);
+vals=[angulos' potenciadBm' potenciadB' potenciaWatts'];
 
 figure(4);
 polarplot(angulo,potenciadBm);
 hold on;
 polarplot(angulo2,potenciadBm);
-title('dBm');
+title('Patron de radiacion dipolo dobadlo [dBm]','Interpreter','latex');
 
 figure(5);
 polarplot(angulo,potenciadB);
 hold on;
 polarplot(angulo2,potenciadB);
-title('dB');
+title('Patron de radiacion dipolo dobadlo [dB]','Interpreter','latex');
 
 figure(6);
 polarplot(angulo,potenciaWatts);
 hold on;
 polarplot(angulo2,potenciaWatts);
-title('Watt');
+title('Patron de radiacion dipolo dobadlo [W]','Interpreter','latex');
 
 
